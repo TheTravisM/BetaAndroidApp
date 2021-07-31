91029f6ebf5562318945068991131af300d5513a
Travis E  Mikolay <TravisEMikolay+GitKraken@Gmail.com>
2021-07-31 13:17:10 -0400

    Initial commit

---
+++ app/src/main/java/com/travism/betaandroidapp/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.travism.betaandroidapp
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
